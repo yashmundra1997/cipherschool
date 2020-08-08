2256e43e7b6e34c4d764363df4efec0aac16e64e
yashmundra1997 <49819869+yashmundra1997@users.noreply.github.com>
2020-08-08 09:05:38 +0530

    Update brackets.cpp

--- brackets.cpp
+++ brackets.cpp
@@ -1,43 +1,60 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
+#include <bits/stdc++.h> 
+using namespace std; 
+
+bool areParanthesisBalanced(string str) 
+{ 
+    stack<char> s; 
+    char x; 
+    for (int i = 0; i < str.size(); i++) { 
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{') { 
+            s.push(str[i]); 
+            continue; 
+        } 
+        if (s.empty()) 
+            return false; 
+  
+        switch (str[i]) { 
+        case ')': 
+  
+            x = s.top(); 
+            s.pop(); 
+            if (x == '{' || x == '[') 
+                return false; 
+            break; 
+  
+        case '}': 
+  
+            x = s.top(); 
+            s.pop(); 
+            if (x == '(' || x == '[') 
+                return false; 
+            break; 
+  
+        case ']': 
+  
+            x = s.top(); 
+            s.pop(); 
+            if (x == '(' || x == '{') 
+                return false; 
+            break; 
+        } 
+    } 
+    return (s.empty()); 
+} 
+  
+ 
+int main() 
+{ 
     int n;
     cin>>n;
     while(n--)
     {
-        string s;
-        cin>>s;
-        stack<char> st;
-        queue<char> q;
-        int f=0;
-        if(s.size()%2!=0) cout<<"NO"<<endl;
-        else
-        {
-            for(int i=0;i<(s.size()/2);i++)
-            {
-                st.push(s[i]);
-            }
-            for(int i=(s.size()/2);i<s.size();i++)
-            {
-                q.push(s[i]);
-            }
-            for(int i=0;i<s.size();i++)
-            {
-                if(st.top()=='{'&&q.front()=='}') continue;
-                if(st.top()=='['&&q.front()==']') continue;
-                if(st.top()=='('&&q.front()==')') continue;
-                else 
-                {
-                    f=1;
-                    break;
-                }
-                q.pop();
-                st.pop();
-            }
-        }
-        if(f==1) cout<<"NO"<<endl;
-        else if(f==0) cout<<"YES"<<endl;
+        string str;
+        cin>>str;
+    if (areParanthesisBalanced(str)) 
+        cout <<"YES"<<endl; 
+    else
+        cout <<"NO"<<endl; 
     }
-    return 0;
-}
+    return 0; 
+} 
